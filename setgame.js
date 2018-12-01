6198df25deaf4eaf908d729e764d40d55c342a58
liasly <40878299+SeongJaeYoon@users.noreply.github.com>
2018-12-02 02:24:03 +0900

    환상체 객체 추가

--- setgame.js
+++ setgame.js
@@ -1,11 +1,101 @@
 var Abnormality = new Array(120);
 
+Abnormality[0] = { name:'교육용 오뚜기 토끼', number:'0-00-00'};
+Abnormality[2] = { name:'불타버린 소녀', number:'F-01-02'};
+Abnormality[3] = { name:'단 한번의 악과 수백 가지의 선행', number:'O-03-03'};
+Abnormality[4] = { name:'증오의 여왕', number:'O-01-04'};
+Abnormality[6] = { name:'행복한 테디', number:'T-04-06'};
+Abnormality[8] = { name:'빨간 구두', number:'O-04-08'};
+Abnormality[9] = { name:'테레지아', number:'T-09-09'};
+Abnormality[12] = { name:'늙은 여인', number:'O-01-12'};
+Abnormality[15] = { name:'이름이 없는 태아', number:'O-01-15'};
+Abnormality[18] = { name:'벽을 보는 여인', number:'F-01-18'};
+Abnormality[20] = { name:'아무 것도 없는', number:'O-06-20'};
+Abnormality[27] = { name:'1.76 MHz', number:'T-06-27'};
+Abnormality[30] = { name:'노래하는 기계', number:'O-05-30'};
+Abnormality[31] = { name:'고요한 오케스트라', number:'T-01-31'};
+Abnormality[32] = { name:'따뜻한 마음의 나무꾼', number:'F-05-32'};
+Abnormality[37] = { name:'눈의 여왕', number:'F-01-37'};
+Abnormality[40] = { name:'큰 새', number:'O-02-40'};
+Abnormality[41] = { name:'무엇이든 도와드립니다', number:'T-05-41'};    
+Abnormality[42] = { name:'백설공주의 사과', number:'F-04-42'};
+Abnormality[43] = { name:'거미봉오리', number:'T-02-43'};
+Abnormality[44] = { name:'미녀와 야수', number:'F-02-44'};
+Abnormality[45] = { name:'역병의사', number:'O-01-45'};
+Abnormality[46] = { name:'백야', number:'T-03-46'};
+Abnormality[47] = { name:'만지지마', number:'O-05-47'};
+Abnormality[49] = { name:'썰매의 루돌타', number:'F-02-49'};
+Abnormality[50] = { name:'여왕벌', number:'T-04-50'};
+Abnormality[51] = { name:'피의 욕조', number:'T-05-51'};
+Abnormality[52] = { name:'뚜껑 따인 웰치어스', number:'F-05-52'};
+Abnormality[53] = { name:'알리우네', number:'T-04-53'};
+Abnormality[54] = { name:'버려진 살인마', number:'T-01-54'};
+Abnormality[55] = { name:'은하수의 아이', number:'O-01-55'};
+Abnormality[56] = { name:'징벌 새', number:'O-02-56'};
+Abnormality[57] = { name:'붉은 두건의 용병', number:'F-01-57'};
+Abnormality[58] = { name:'커다랗고 나쁠 늑대', number:'F-02-58'};
+Abnormality[59] = { name:'너 대머리야...', number:'Bald-is-Awesome!'};
+Abnormality[60] = { name:'우주 속의 조각', number:'O-03-60'};
+Abnormality[61] = { name:'부서져 가는 갑옷', number:'O-05-61'};
+Abnormality[62] = { name:'심판 새', number:'O-02-62'};
+Abnormality[63] = { name:'종말새', number:'O-02-63'};
+Abnormality[64] = { name:'탐욕의 왕', number:'O-01-64'};
+Abnormality[66] = { name:'어린 왕자', number:'O-04-66'};
+Abnormality[67] = { name:'래티씨아', number:'O-01-67'};
+Abnormality[68] = { name:'죽은 나비들의 장례', number:'T-01-68'};
+Abnormality[69] = { name:'마탄의 사수', number:'F-01-69'};
+Abnormality[70] = { name:'흑조의 꿈', number:'F-02-70'};
+Abnormality[71] = { name:'꿈꾸는 해류', number:'T-02-71'};
+Abnormality[72] = { name:'파고드는 천국', number:'O-04-72'};
+Abnormality[73] = { name:'절망의 기사', number:'O-01-73'};
+Abnormality[74] = { name:'벌거벗은 둥지', number:'O-02-74'};
+Abnormality[75] = { name:'웃는 시체들의 산', number:'T-01-75'};
+Abnormality[76] = { name:'샤덴프로이데', number:'O-05-76'};
+Abnormality[77] = { name:'열망하는 심장', number:'T-09-77'};
+Abnormality[78] = { name:'미쳐버린 연구원의 수첩', number:'T-09-78'};
+Abnormality[79] = { name:'살점 우상', number:'T-09-79'};
+Abnormality[80] = { name:'거목수액', number:'T-09-80'};
+Abnormality[81] = { name:'조정의 거울', number:'O-09-81)'};
+Abnormality[82] = { name:'3월 27일의 방공호', number:'T-09-82'};
+Abnormality[83] = { name:'요정의 축제', number:'F-04-83'};
+Abnormality[84] = { name:'고기 초롱', number:'T-09-85'};
+Abnormality[85] = { name:'무엇이든 바꿔드립니다', number:'T-09-85'};
+Abnormality[86] = { name:'지옥행 급행열차', number:'T-09-86'};
+Abnormality[87] = { name:'지혜를 구하는 허수아비', number:'F-01-87'};
+Abnormality[88] = { name:'차원굴절 변이체', number:'O-03-88'};
+Abnormality[89] = { name:'검열삭제', number:'O-03-89'};
+Abnormality[90] = { name:'살갗 예언', number:'T-09-90'};
+Abnormality[91] = { name:'이계의 초상', number:'O-09-91'};
+Abnormality[92] = { name:'수줍은 오늘의 표정', number:'O-01-92'};
+Abnormality[93] = { name:'푸른 별', number:'O-03-93'};
+Abnormality[94] = { name:'당신은 행복해야합니다.', number:'T-09-94'};
+Abnormality[95] = { name:'발광 팔찌', number:'O-09-95'};
+Abnormality[96] = { name:'행동교정', number:'O-09-96'};
+Abnormality[97] = { name:'오래전의 믿음과 약속', number:'T-09-97'};
+Abnormality[98] = { name:'포큐버스', number:'O-02-98'};
+Abnormality[99] = { name:'공허한 꿈', number:'T-02-99'};
+Abnormality[100] = { name:'벚꽃의 무덤', number:'O-04-100'};
+Abnormality[101] = { name:'불새', number:'O-02-101'};
+Abnormality[102] = { name:'음', number:'O-05-102'};
+Abnormality[103] = { name:'양', number:'O-07-103'};
+Abnormality[104] = { name:'거꾸로 가는 시계', number:'D-09-104'};
+Abnormality[105] = { name:'달의 울음', number:'D-01-105'};
+Abnormality[106] = { name:'검은 군단', number:'D-01-106'};
+Abnormality[107] = { name:'뽀대', number:'D-02-107'};
+Abnormality[108] = { name:'기생수', number:'D-04-108'};
+Abnormality[109] = { name:'녹아내리는 사랑', number:'D-03-109'};
+Abnormality[110] = { name:'풍운승려', number:'D-01-110'};
+
 var rightab = new Array(60);
 var leftab = new Array(60);
 
 var N = getCookie('gang');
 
-console.log(N);
+var right_p = document.getElementById("right_portrait");
+var right_I = document.getElementById("right_ingame");
+
+var left_p = document.getElementById("left_portrait");
+var left_I = document.getElementById("left_ingame");
 
 setGame(N);
 function checking(arr, num, now){
@@ -15,7 +105,7 @@ function checking(arr, num, now){
         if(arr[i] == num) check = 0;    // 중복체크
     }
 
-    //if(Abnormality[num] == null) check = 0; // null체크
+    if(Abnormality[num] == null) check = 0; // null체크
 
     return check;  
 
@@ -42,8 +132,23 @@ function setGame(n){
 
 }
 
-
-var getCookie = function(name) {
+function getCookie(name) {
     var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
     return value? value[2] : null;
-  }
+}
+
+function WhoW(obj){
+    if(obj.id == 'right') rightab.shift();
+    if(obj.id == 'left') leftab.shift();
+
+    // if(rightab.length + leftab == 1)
+}
+
+
+
+
+
+
+
+
+
